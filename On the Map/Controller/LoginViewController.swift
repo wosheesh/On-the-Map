a2152fb4dc608208184fcc5a2288180a28ed920a
wmaterka <w.materka@gmail.com>
2016-01-10 17:39:41 +1100

    init

---
+++ On the Map/Controller/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  On the Map
+//
+//  Created by Wojtek Materka on 10/01/2016.
+//  Copyright © 2016 Wojtek Materka. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+}
