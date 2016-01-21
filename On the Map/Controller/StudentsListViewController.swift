70ab28f8b57f4c082d0156a48ecfdf00cca6333f
wmaterka <w.materka@gmail.com>
2016-01-21 16:50:16 +0100

    topnavbar hell

---
+++ On the Map/Controller/StudentsListViewController.swift
@@ -0,0 +1,26 @@
+//
+//  StudentsListViewController.swift
+//  On the Map
+//
+//  Created by Wojtek Materka on 21/01/2016.
+//  Copyright © 2016 Wojtek Materka. All rights reserved.
+//
+
+import UIKit
+
+
+class StudentsListViewController: UIViewController {
+    override func viewDidLoad() {
+        
+        let button1 = UIBarButtonItem(title: "Logout", style: .Plain, target: self, action: "Logout")
+        let button2 = UIBarButtonItem(title: "Pin", style: .Plain, target: self, action: "SetStudentLocation")
+        let button3 = UIBarButtonItem(title: "Refresh", style: .Plain, target: self, action: "RefreshStudentData")
+        
+        navigationItem.hidesBackButton = true
+        
+        navigationItem.leftItemsSupplementBackButton = true
+        navigationItem.setLeftBarButtonItem(button1, animated: true)
+        navigationItem.setRightBarButtonItems([button2, button3], animated: true)
+        
+    }
+}
